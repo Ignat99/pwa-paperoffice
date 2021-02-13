490f128aefc3a8087618a735a741ecf7753d0243
DimitriiIgnatev <46378328+DimitriiIgnatev@users.noreply.github.com>
2021-02-13 20:00:58 +0100

    search 2

--- src/ContactMemoryTable.js
+++ src/ContactMemoryTable.js
@@ -9,23 +9,6 @@ export default class ContactMemoryTable extends MemoryTable {
   render() {
     const items = this.props.items;
       
-      
-    const tablePropsData = [
-            ["mode", "normal", "Table mode: <code>normal</code>, <code>static</code>. When static, sortable classes will not be drawn."],
-            ["head", "null", "Table head. This is a array of table head cells"],
-            ["body", "null", "Table body. This is a array of table body cells"],
-            ["cls, className", "empty", "Table classes"],
-            ["clsHeadRow", "empty", "Table classes for head row"],
-            ["clsHeadCell", "empty", "Table classes for head cell"],
-            ["clsBodyRow", "empty", "Table classes for body row"],
-            ["clsBodyCell", "empty", "Table classes for body cell"],
-            ["onHeadClick", "()=>{}", "Callback for head cell click event"],
-            ["onCellClick", "()=>{}", "Callback for body cell click event"],
-            ["onDrawCell", "()=>{}", "Callback for body cell draw event"],
-        ];
-      
-      
-    
     
    const tablePropsHeader =  [
  /*   {        
@@ -67,14 +50,8 @@ export default class ContactMemoryTable extends MemoryTable {
     
     return (
     <Container>
-        <MemoryTable id='Memorytable' className="table striped table-border mt-4" rows="5" pagination="true" search="true" head={tablePropsHeader} body={items.map(item => [
-                  item.username,
-                  item.email,
-                  item.birthday,
-                  item.checkbox,
-                  item.picture,
-                ]
-            )} scrollable="true" />
+        <MemoryTable id='Memorytable' className="table striped table-border mt-4" rows="5" pagination="true" search="true" head={tablePropsHeader} 
+            body={items.map(item => [item.username, item.email, item.birthday, item.checkbox, item.picture ])} scrollable="true" />
     </Container>
             )
     }
