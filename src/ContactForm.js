3cee32e118379bc66aae23c98a623b85412ab02d
DimitriiIgnatev <46378328+DimitriiIgnatev@users.noreply.github.com>
2021-02-14 15:02:21 +0100

    input onChange

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Checkbox, ImageButton, Icon, Container } from 'metro4-react';
+import { Input, Row, Checkbox, ImageButton, Icon, Container } from 'metro4-react';
 import CapturePicturePlayer from './CapturePicturePlayer';
 
 class ContactForm extends Component {
@@ -13,55 +13,31 @@ class ContactForm extends Component {
     
     render() {
         return(
-                    <div id="ContactForm">
-                      <form onSubmit={this.props.handleFormSubmit} >
-                        <Container>
-                            <div data-role="tile" data-size="small">
-                                <Icon sizes="51,51" 
-                                    src="future-512.png" prefix="fa mif-" cls="icon"/>
-                            </div>
-                            <div data-role="tile" data-size="large">
-                                <CapturePicturePlayer newPictureURL={ this.handleNewPicture } />  
-                            </div>
-                        </Container>
-
-        <label htmlFor="username">
-            Username:
-          <input id="username" value={this.props.newUsername} 
-            type="text" name="username"  
-                onChange={this.props.handleInputChange} />
-        </label>
-            
-        <label htmlFor="email">
-          Email:
-          <input id="email" value={this.props.newEmail} 
-            type="text" name="email" 
-                onChange={this.props.handleInputChange} />
-        </label>     
-
-            
-            
-
-        <label htmlFor="birthday">
-            Birthday:
-            <input id="birthday" value={this.props.newBirthday}
-                type="text" name="birthday" 
-                onCange={this.props.handleInputChange} />
-        </label>
-            
-        <label htmlFor="checkbox">
-            Remember me:
-            <Checkbox id="checkbox" name="checkbox" 
-                value={this.props.newCheckbox} onCange={this.props.handleInputChange} />
-        </label>
-                            
-
-            <ImageButton type="submit" title="Submit form" className='.image-button:focus'>
-                                <Icon name="rocket" prefix="fa fa-" cls="icon"/> 
-                                <span className="caption"></span>
-            </ImageButton>
-                      </form>
-                    </div>
+            <div id="ContactForm">
+                <form onSubmit={this.props.handleFormSubmit} >
+                    <Container>
+                        <div data-role="tile" data-size="small">
+                            <Icon sizes="51,51" 
+                                src="future-512.png" prefix="fa mif-" cls="icon"/>
+                        </div>
+                        <div data-role="tile" data-size="large">
+                            <CapturePicturePlayer newPictureURL={ this.handleNewPicture } />  
+                        </div>
+                    </Container>
+                        <Input id="username" type="text" name="username" prepend="Name: " cls="fa-user" placeholder="User name" 
+                            value={this.props.newUsername} onChange={this.props.handleInputChange} />
+                        <Input id="email" type="text" name="email" prepend="Email: " cls="mif-envelop" placeholder="Enter your email..." data-validate="required email" 
+                            value={this.props.newEmail} onChange={this.props.handleInputChange}  />
+                        <Input id="birthday" type="date" name="birthday" 
+                            value={this.props.mewBirthday} onChange={this.props.handleInputChange}  />
+                        <Checkbox caption="Remember me"
+                            value={this.props.newCheckbox} onChange={this.props.handleInputChange}  />
+                        <ImageButton type="submit" title="Submit form" className='.image-button:focus'>
+                            <Icon name="rocket" prefix="fa fa-" cls="icon"/> 
+                            <span className="caption"></span>
+                        </ImageButton>
+                </form>
+            </div>
         );
     }
 }
